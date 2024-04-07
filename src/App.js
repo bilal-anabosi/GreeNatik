22078fcece7ed899f4c15482aa4a4fbdf688a93b
bilal anabosi <bolbol.anabosi.1@gmail.com>
2024-04-07 20:59:57 +0200

    new react file

---
+++ src/App.js
@@ -0,0 +1,25 @@
+
+import './App.css';
+import Navbar from './components/Navbar';
+import Home from './pages/Test';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+} from "react-router-dom";
+
+function App() {
+  return (
+    <>
+    <Navbar />
+      <Router>
+        <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        </Routes>
+      </Router>
+      </>
+  );
+}
+
+export default App;

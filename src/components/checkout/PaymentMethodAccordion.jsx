3a0eb4eac7a4fa9b99535239c1ccbc706c6a89a6
bilal anabosi <bolbol.anabosi.1@gmail.com>
2024-04-12 22:20:50 +0200

    removed unnecessary files

--- src/components/checkout/PaymentMethodAccordion.jsx
+++ src/components/checkout/PaymentMethodAccordion.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
-import 'react-datepicker/dist/react-datepicker.css';
+
 
 function PaymentMethodAccordion({ onUpdate }) {
   const [selectedPaymentMethod, setSelectedPaymentMethod] = useState("paypal");

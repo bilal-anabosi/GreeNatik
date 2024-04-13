0516926bd583b9a172806c66ba4599faf83d278b
SanaMahmoodd <s12217400@stu.najah.edu>
2024-04-13 21:41:20 +0300

    Update HomePage(MainStore)

---
+++ src/pages/Store.jsx
@@ -0,0 +1,25 @@
+// src/pages/Home.js
+import React from 'react';
+import HomeBar from '../components/Home/HomeBar/HomeBar';
+import Slide from '../components/Home/Slides/Slide';
+import Feature from '../components/Home/Feature/Feature';
+import Cards from '../components/Home/Categories/Cards/Cards';
+const Store = () => {
+  return (
+   
+
+<div className='Homee'>
+
+<HomeBar/>
+<Slide/>
+<Feature/>
+<Cards/>
+
+</div>
+
+
+
+  );
+};
+
+export default Store;

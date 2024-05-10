e092e3fb2b184584241b5e0e83b3ae840d03cb75
roaa-kittaneh <roaakittaneh2004@gmail.com>
2024-05-10 16:55:04 +0300

    auth and profile

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,25 @@
 const express = require('express');
 const cors = require('cors');
 // Make sure the path matches the location of your productRoutes file
+
 const productRoutes = require('./routes/productRoutes'); 
+const userRouter = require('./routes/user.router.js');
+const profileRouter = require('./profile/profile.router.js');
+
 const connectDB = require('./DB/connection');
 require('dotenv').config();
 
 
 const app = express();
+app.use(express.json());
 const PORT = process.env.PORT || 4000;
 
 connectDB();
 app.use(cors());
-app.use(express.json()); // for parsing application/json
 
 app.use('/api/products', productRoutes);
+app.use('/user', userRouter);
+app.use('/profile', profileRouter);
 
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);

e9fd79a5012f939f2f932e16404bd5763c9d145c
roaa-kittaneh <roaakittaneh2004@gmail.com>
2024-05-10 22:48:13 +0300

    add validation file

--- backend/routes/user.router.js
+++ backend/routes/user.router.js
@@ -1,10 +1,13 @@
 const { Router } = require("express");
 const controller = require('../controllers/user.controller.js');
+const validation2 = require('../middelware/validation.js');
+const { signinschema, signupschema } = require('../controllers/user.validation.js');
+
 
 
 const router = Router();
-router.post('/signup', controller.signup);
-router.post('/login', controller.login);
+router.post('/signup',validation2(signupschema), controller.signup);
+router.post('/login',validation2(signinschema), controller.login);
 router.patch('/sendcode',controller.sendcode);
 router.patch('/forgetpassword',controller.forgetpassword);
 

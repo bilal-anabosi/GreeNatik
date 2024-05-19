528f8d7d31f9859004f06a6d1c33aea614eacf83
Toqaasedah3 <s12218467@stu.najah.edu>
2024-05-19 04:06:40 +0300

    front

--- frontend/src/components/Home/Categories/Wishlist/WishlistTable.jsx
+++ frontend/src/components/Home/Categories/Wishlist/WishlistTable.jsx
@@ -1,80 +1,58 @@
 import React, { useState, useEffect } from "react";
+import axios from 'axios';
 
 const WishlistTable = () => {
   const [wishlist, setWishlist] = useState([]);
-  const [selectAll, setSelectAll] = useState(false);
+  const token = localStorage.getItem('userToken');
 
   useEffect(() => {
     const fetchWishlistData = async () => {
-      const fetchedData = [
-        {
-          id: 1,
-          image: process.env.PUBLIC_URL + '/pics/product-img-18.png',
-          name: "Organic Banana",
-          price: "$35.00",
-          status: "In Stock",
-          selected: false,
-        },
-        {
-          id: 2,
-          image: process.env.PUBLIC_URL + '/pics/product-img-17.png',
-          name: "Fresh Kiwi",
-          price: "$20.97",
-          status: "Out of Stock",
-          selected: false,
-        },
-        {
-          id: 3,
-          image: process.env.PUBLIC_URL + '/pics/product-img-16.png',
-          name: "Golden Pineapple",
-          price: "$35.00",
-          status: "In Stock",
-          selected: false,
-        },
-        {
-          id: 4,
-          image: process.env.PUBLIC_URL + '/pics/product-img-19.png',
-          name: "BeatRoot",
-          price: "$29.00",
-          status: "In Stock",
-          selected: false,
-        },
-        {
-          id: 5,
-          image: process.env.PUBLIC_URL + '/pics/product-img-15.png',
-          name: "Fresh Apple",
-          price: "$70.00",
-          status: "In Stock",
-          selected: false,
-        },
-      ];
+      if (!token) {
+        console.error('No token found');
+        return;
+      }
 
-      setWishlist(fetchedData);
+      try {
+        const response = await axios.get('http://localhost:4000/wishlist', {
+          headers: {
+            'Authorization': `group__${token}`
+          }
+        });
+        setWishlist(response.data.products);
+      } catch (error) {
+        console.error('Error fetching wishlist:', error);
+      }
     };
 
     fetchWishlistData();
-  }, []);
+  }, [token]);
 
-  const handleSelectAll = () => {
-    setSelectAll(!selectAll);
-    const updatedWishlist = wishlist.map((item) => ({
-      ...item,
-      selected: !selectAll,
-    }));
-    setWishlist(updatedWishlist);
-  };
+  const handleDeleteItem = async (id) => {
+    if (!token) {
+      console.error('No token found');
+      return;
+    }
 
-  const handleSelectItem = (id) => {
-    const updatedWishlist = wishlist.map((item) =>
-      item.id === id ? { ...item, selected: !item.selected } : item
-    );
-    setWishlist(updatedWishlist);
-    setSelectAll(updatedWishlist.every((item) => item.selected));
-  };
+    console.log(`Attempting to delete item with ID: ${id} `);
 
-  const handleDeleteItem = (id) => {
-    const updatedWishlist = wishlist.filter((item) => item.id !== id);
-    setWishlist(updatedWishlist);
+    try {
+      const response = await axios.delete(`http://localhost:4000/wishlist/${id}`, {
+        headers: {
+          'Authorization': `group__${token}`,
+        },
+      
+      });
+
+      if (response.status === 200) {
+        const updatedWishlist = wishlist.filter((item) => item._id !== id);
+        setWishlist(updatedWishlist);
+        console.log(`Item with ID: ${id}  successfully deleted`);
+      } else {
+        console.error(`Failed to delete item with ID: ${id}  status code: ${response.status}`);
+      }
+    } catch (error) {
+      console.error(`Error deleting item with ID: ${id} `, error);
+    }
   };
 
   return (
@@ -84,28 +62,12 @@ const WishlistTable = () => {
           <div className="col-lg-12">
             <div className="mb-8">
               <h1 className="mb-1">My Wishlist</h1>
-              <p>There are 5 products in this wishlist.</p>
+              <p>There are {wishlist.length} products in this wishlist.</p>
             </div>
             <div className="table-responsive">
               <table className="table text-nowrap table-padding">
                 <thead className="table-light">
                   <tr>
-                    <th>
-                      <div className="form-check">
-                        <input
-                          className="form-check-input"
-                          type="checkbox"
-                          value=""
-                          id="checkAll"
-                          checked={selectAll}
-                          onChange={handleSelectAll}
-                        />
-                        <label
-                          className="form-check-label"
-                          htmlFor="checkAll"
-                        ></label>
-                      </div>
-                    </th>
                     <th></th>
                     <th>Product</th>
                     <th>Amount</th>
@@ -116,22 +78,11 @@ const WishlistTable = () => {
                 </thead>
                 <tbody>
                   {wishlist.map((item) => (
-                    <tr key={item.id}>
-                      <td className="align-middle">
-                        <div className="form-check">
-                          <input
-                            className="form-check-input"
-                            type="checkbox"
-                            value=""
-                            checked={item.selected}
-                            onChange={() => handleSelectItem(item.id)}
-                          />
-                        </div>
-                      </td>
+                    <tr key={item._id}>
                       <td className="align-middle">
                         <a href="#1">
                           <img
-                            src={item.image}
+                            src={item.product.images && item.product.images[0] ? `http://localhost:4000/${item.product.images[0]}` : 'path/to/default-image.jpg'}
                             className="icon-shape icon-xxl"
                             alt=""
                           />
@@ -141,38 +92,44 @@ const WishlistTable = () => {
                         <div>
                           <h5 className="fs-6 mb-0">
                             <a href="#1" className="text-inherit">
-                              {item.name}
+                              {item.product.title}
                             </a>
                           </h5>
-                          <small>{item.price}</small>
+                          <small>{item.product.price}</small>
                         </div>
                       </td>
-                      <td className="align-middle">{item.price}</td>
+                      <td className="align-middle">{item.product.amount}</td>
                       <td className="align-middle">
                         <span
-                          className={`badge ${
-                            item.status === "In Stock"
-                              ? "bg-success"
-                              : "bg-danger"
-                          }`}
+                          className={`badge ${item.product.status ? "bg-success" : "bg-danger"}`}
                         >
-                          {item.status}
+                          {item.product.status ? "In Stock" : "Out of Stock"}
                         </span>
                       </td>
                       <td className="align-middle">
                         <a href="/cart" className="btn btn-primary btn-sm">Add to Cart</a>
                       </td>
-                      <td className="align-middle ">
-                        <a
-                          href="#!"
-                          className="text-muted"
-                          data-bs-toggle="tooltip"
-                          data-bs-placement="top"
-                          aria-label="Delete"
-                          onClick={() => handleDeleteItem(item.id)}
-                        >
+                      <td className="align-middle">
+                      <button
+  href="!"
+  className="text-muted"
+  data-bs-toggle="tooltip"
+  data-bs-placement="top"
+  aria-label="Delete"
+  style={{
+    border: 'none',
+    background: 'none',
+    padding: '0',
+    cursor: 'pointer',
+    outline: 'none',
+    boxShadow: 'none' 
+  }}
+  onClick={() => handleDeleteItem(item._id)}
+>
+
+
                           <i className="bi bi-trash"></i>
-                        </a>
+                        </button>
                       </td>
                     </tr>
                   ))}

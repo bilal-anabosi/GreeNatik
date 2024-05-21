0cf1f9c0e26b8feea6adc962f53f7662fe761134
Toqaasedah3 <s12218467@stu.najah.edu>
2024-05-21 15:26:09 +0300

    checkoutdone

--- backend/controllers/checkoutController.js
+++ backend/controllers/checkoutController.js
@@ -1,8 +1,22 @@
 const Checkout = require('../models/CheckoutModel');
 const User = require('../models/usermodel');
 
+// Function to generate random order number
+const generateOrderNumber = () => {
+  const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const digits = "0123456789";
+  let orderNumber = "";
+  for (let i = 0; i < 2; i++) {
+    orderNumber += letters.charAt(Math.floor(Math.random() * letters.length));
+  }
+  for (let i = 0; i < 2; i++) {
+    orderNumber += digits.charAt(Math.floor(Math.random() * digits.length));
+  }
+  return orderNumber;
+};
+
 async function createCheckout(req, res) {
-  const { address, deliveryInstructions, paymentMethod, items } = req.body;
+  const { address, deliveryInstructions, paymentMethod, items, totalAfterDiscount } = req.body;
 
   if (!req.user || !req.user.id) {
     return res.status(401).json({ message: "User not authenticated" });
@@ -17,6 +31,8 @@ async function createCheckout(req, res) {
       deliveryInstructions: deliveryInstructions,
       paymentMethod: paymentMethod,
       items: items,
+      numOrder: generateOrderNumber(), // Generates the random order number
+      total: totalAfterDiscount // Store the total after discount
     });
 
     const user = await User.findById(userId);

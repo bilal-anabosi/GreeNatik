549be5073413b96df2f7a87fbca9b1eef02fe6b4
roaa-kittaneh <roaakittaneh2004@gmail.com>
2024-05-24 03:02:20 +0300

    add error notification

--- frontend/src/pages/account/Forms/LoginForm.jsx
+++ frontend/src/pages/account/Forms/LoginForm.jsx
@@ -1,36 +1,42 @@
 import FormGenerator from './FormGenerator';
 import * as yup from 'yup';
-import { useContext, useEffect } from 'react'; 
+import React, { useState,useContext } from 'react';
 import { UserContext } from '../context/User.jsx';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
-import React from 'react';
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 export default function LoginForm() {
   const navigate = useNavigate();
   const { userToken, setUserToken } = useContext(UserContext) || {};
-  
-  useEffect(() => {
-    if (userToken) {
-      navigate(-1);
-    }
-  }, [userToken, navigate]);
+  const [errorMessage, setErrorMessage] = useState('');
+  const [successMessage, setSuccessMessage] = useState('');
+
 
   const onSubmit = async (userData) => {
     try {
       const { data, status } = await axios.post(`http://localhost:4000/user/login`, userData);
       console.log(data);
-  
+
       if (status === 200) {
         localStorage.setItem('userToken', data.token);
         setUserToken(data.token);
-        
-        navigate('/store');
+        setSuccessMessage('Login successful! Redirecting...');
+        setErrorMessage('');
+
+        if (data.user && data.user.role === 'admin') {
+          navigate('/store');
+        } else {
+          navigate('/store');
+        }
       } else if (status === 401) {
-        console.log("error 401");
+        setErrorMessage('Invalid email or password');
       }
     } catch (error) {
       console.error('Error during login:', error);
+      setErrorMessage('Invalid email or password');
+      setSuccessMessage('');
     }
   };
 
@@ -52,8 +58,20 @@ export default function LoginForm() {
   ];
 
   return (
-    <>
+    <div>
+      {errorMessage && (
+        <div className="alert alert-danger alert-dismissible fade show" role="alert">
+          {errorMessage}
+          <button type="button" className="btn-close" onClick={() => setErrorMessage('')} aria-label="Close"></button>
+        </div>
+      )}
+       {successMessage && (
+        <div className="alert alert-success alert-dismissible fade show" role="alert">
+          {successMessage}
+          <button type="button" className="btn-close" onClick={() => setSuccessMessage('')} aria-label="Close"></button>
+        </div>
+      )}
       <FormGenerator onSubmit={onSubmit} attributes={attributes} />
-    </>
+    </div>
   );
 }
